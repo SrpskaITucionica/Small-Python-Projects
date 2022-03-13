3f9b771fdc174c383931fc56fa02799b756c32ba
Srpska IT učionica <101505320+SrpskaITucionica@users.noreply.github.com>
2022-03-13 13:08:19 +0100

    Add files via upload

---
+++ dice roll.py
@@ -0,0 +1,19 @@
+import random
+
+
+def roll_dices_game():
+    while True:
+        dice_one = random.randint(1, 6)  # Generates number in range 1-6
+        dice_two = random.randint(1, 6)  # Generates number in range 1-6
+        print(f' You got {dice_one} and {dice_two}.')
+        response = input('''
+        Do you want to role again? 
+        y - yes
+        n - no
+        > ''')
+        if response.lower() == 'y':
+            continue
+        else:
+            break
+
+
